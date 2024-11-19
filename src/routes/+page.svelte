120e55d12c942f805942f793f28b8ad47b673815
Mapagmataas <98333916+Mapagmataas1331@users.noreply.github.com>
2024-11-20 02:47:53 +0700

    Big styling update + Calendar ru translation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,15 +2,15 @@
 	import { onMount } from 'svelte';
 	import * as Select from '$shared/components/ui/select';
 	import { RangeCalendar } from '$shared/components/ui/range-calendar/index.js';
+	import Button from '$shared/components/ui/button/button.svelte';
 	import * as Alert from '$shared/components/ui/alert/index.js';
 	import * as Table from '$shared/components/ui/table';
+	import Separator from '$shared/components/ui/separator/separator.svelte';
 	import Skeleton from '$shared/components/ui/skeleton/skeleton.svelte';
-	import Button from '$shared/components/ui/button/button.svelte';
 	import Ban from 'lucide-svelte/icons/ban';
 	import Sticker from 'lucide-svelte/icons/sticker';
 	import MousePointerClick from 'lucide-svelte/icons/mouse-pointer-click';
-	import Plus from 'lucide-svelte/icons/plus';
-	import Minus from 'lucide-svelte/icons/minus';
+	import Calendar from 'lucide-svelte/icons/calendar';
 	import { language } from '$shared/stores/language';
 	import { viewport, breakpoints } from '$shared/stores/viewport';
 	import { fetchTableData } from '$lib/utils/fetchTableData';
@@ -23,11 +23,6 @@
 		discipline: string;
 		auditorium: string;
 		teacher: string;
-	};
-
-	type ScheduleEntry = {
-		dayOfWeek: string;
-		lessons: Lesson[];
 	};
 
 	let selectedYear = $state('');
@@ -50,7 +45,7 @@
 
 	let yearOptions: string[] = ['1', '2', '3', '4'];
 	let groupOptions: { [key: string]: number } = $state({});
-	let buildedSchedule: Record<string, ScheduleEntry> = $state({});
+	let buildedSchedule: Record<string, Lesson[]> = $state({});
 
 	let groupVisible = $state(false);
 	let rangeVisible = $state(false);
@@ -116,13 +111,12 @@
 			currentDate.setDate(currentDate.getDate() + 1);
 		}
 
-		buildedSchedule = dates.reduce((acc: Record<string, ScheduleEntry>, date) => {
+		buildedSchedule = dates.reduce((acc: Record<string, Lesson[]>, date) => {
 			if (!schedule) {
 				scheduleError = 'Error: Schedule is null';
 				scheduleStatus = 'error';
 				return {};
 			}
-			const dayOfWeek = getDayOfWeek(date);
 			const isStudyDay = studyDates[date] !== undefined;
 
 			let lessons: Lesson[] = [];
@@ -148,7 +142,7 @@
 				}
 			}
 
-			acc[date] = { dayOfWeek, lessons };
+			acc[date] = lessons;
 			return acc;
 		}, {});
 
@@ -225,16 +219,21 @@
 		}, {});
 	};
 
-	const getDayOfWeek = (dateStr: string) => {
-		const daysOfWeek = [
-			'Воскресенье',
-			'Понедельник',
-			'Вторник',
-			'Среда',
-			'Четверг',
-			'Пятница',
-			'Суббота'
-		];
+	const getDayOfWeek = (dateStr: string, lang = 'ru') => {
+		let daysOfWeek = [];
+		if (lang === 'ru') {
+			daysOfWeek = [
+				'Воскресенье',
+				'Понедельник',
+				'Вторник',
+				'Среда',
+				'Четверг',
+				'Пятница',
+				'Суббота'
+			];
+		} else {
+			daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+		}
 		const [day, month, year] = dateStr.split('.').map(Number);
 		const date = new Date(year, month - 1, day);
 		return daysOfWeek[date.getDay()] || 'Неизвестный день!';
@@ -292,101 +291,84 @@
 </script>
 
 <div
-	class="mx-auto flex min-h-[calc(100dvh_-_3.5rem)] w-full flex-col items-center justify-center bg-gradient-to-br from-white to-sky-200 px-1 dark:from-sky-800 dark:to-black md:min-h-[calc(100dvh_-_4rem)]"
+	class="{rangeVisible
+		? 'justify-start'
+		: 'justify-center'} mx-auto flex min-h-[calc(100dvh_-_3.5rem)] w-full flex-col items-center bg-gradient-to-br from-white to-sky-200 px-1 dark:from-sky-800 dark:to-black md:min-h-[calc(100dvh_-_4rem)]"
 >
-	<Select.Root
-		type="single"
-		name="yearSelect"
-		bind:value={selectedYear}
-		onValueChange={handleYearChange}
-	>
-		<Select.Trigger
-			class="my-1 w-full min-w-[256px] bg-background shadow-md md:w-2/3 lg:w-1/2 2xl:w-1/3"
-		>
-			{selectedYear
-				? selectedYear + ' ' + ($language === 'ru' ? 'курс' : 'year')
-				: $language === 'ru'
-					? 'Выберите курс'
-					: 'Select year'}
-		</Select.Trigger>
-		<Select.Content>
-			<Select.Item value="">{$language === 'ru' ? 'Сброс' : 'Reset'}</Select.Item>
-			{#each yearOptions as option}
-				<Select.Item value={option}>{option} {$language === 'ru' ? 'курс' : 'year'}</Select.Item>
-			{/each}
-		</Select.Content>
-	</Select.Root>
-
-	{#if groupVisible}
-		<Select.Root
-			type="single"
-			name="groupSelect"
-			bind:value={selectedGroup}
-			onValueChange={handleGroupChange}
-		>
-			<Select.Trigger
-				class="my-1 w-full min-w-[256px] bg-background shadow-md md:w-2/3 lg:w-1/2 2xl:w-1/3"
+	<div class="m-1 mt-3 flex w-full min-w-[256px] flex-col gap-y-2 md:w-2/3 lg:w-1/2 2xl:w-1/3">
+		<div class="flex flex-col gap-2 md:flex-row">
+			<Select.Root
+				type="single"
+				name="yearSelect"
+				bind:value={selectedYear}
+				onValueChange={handleYearChange}
 			>
-				{selectedGroup ? selectedGroup : $language === 'ru' ? 'Выберите группу' : 'Select group'}
-			</Select.Trigger>
-			<Select.Content class="max-h-[256px] overflow-y-auto">
-				{#each Object.keys(groupOptions) as group}
-					<Select.Item value={group}>{group}</Select.Item>
-				{/each}
-			</Select.Content>
-		</Select.Root>
-	{/if}
+				<Select.Trigger class="w-full bg-background shadow-md">
+					{selectedYear
+						? selectedYear + ' ' + ($language === 'ru' ? 'курс' : 'year')
+						: $language === 'ru'
+							? 'Выберите курс'
+							: 'Select year'}
+				</Select.Trigger>
+				<Select.Content>
+					<Select.Item value="">{$language === 'ru' ? 'Сброс' : 'Reset'}</Select.Item>
+					{#each yearOptions as option}
+						<Select.Item value={option}>{option} {$language === 'ru' ? 'курс' : 'year'}</Select.Item
+						>
+					{/each}
+				</Select.Content>
+			</Select.Root>
 
-	{#if rangeVisible}
-		{@const numberOfMonths = $viewport.vw >= breakpoints.md ? 2 : 1}
-		{#key numberOfMonths}
-			<RangeCalendar
-				bind:value={selectedRange}
-				weekStartsOn={1}
-				{numberOfMonths}
-				class="my-1 items-center justify-center rounded-md border bg-background px-4 shadow-md"
-				onValueChange={handleRangeChange}
-			/>
-		{/key}
-		<p class="mb-1 drop-shadow-xl">
-			{#if selectedRange.start && selectedRange.end}
-				{#if dateValueToString(selectedRange.start) === dateValueToString(selectedRange.end)}
-					<Button
-						variant="ghost"
-						size="icon"
-						onclick={() => {
-							selectedRange = {
-								start: selectedRange.start?.subtract({ days: 1 }),
-								end: selectedRange.end?.subtract({ days: 1 })
-							};
-							buildSchedule();
-						}}
-					>
-						<Minus />
-					</Button>
-					{dateValueToString(selectedRange.start)}
-					<Button
-						variant="ghost"
-						size="icon"
-						onclick={() => {
-							selectedRange = {
-								start: selectedRange.start?.add({ days: 1 }),
-								end: selectedRange.end?.add({ days: 1 })
-							};
-							buildSchedule();
-						}}
-					>
-						<Plus />
-					</Button>
-				{:else}
-					{$language === 'ru' ? 'Выбранный период:' : 'Selected Range:'}
-					{dateValueToString(selectedRange.start)} - {dateValueToString(selectedRange.end)}
-				{/if}
-			{:else}
-				{$language === 'ru' ? 'Период не выбран' : 'Range not selected'}
+			{#if groupVisible}
+				<Select.Root
+					type="single"
+					name="groupSelect"
+					bind:value={selectedGroup}
+					onValueChange={handleGroupChange}
+				>
+					<Select.Trigger class="w-full bg-background">
+						{selectedGroup
+							? selectedGroup
+							: $language === 'ru'
+								? 'Выберите группу'
+								: 'Select group'}
+					</Select.Trigger>
+					<Select.Content class="max-h-[256px] overflow-y-auto">
+						{#each Object.keys(groupOptions) as group}
+							<Select.Item value={group}>{group}</Select.Item>
+						{/each}
+					</Select.Content>
+				</Select.Root>
 			{/if}
-		</p>
-	{/if}
+		</div>
+
+		{#if rangeVisible}
+			{@const numberOfMonths = $viewport.vw >= breakpoints.md ? 2 : 1}
+			{#key numberOfMonths}
+				<div
+					class="group flex min-h-9 w-full flex-col items-center justify-center rounded-md border-input bg-background px-3 py-2 font-medium shadow-md"
+				>
+					<div class="flex w-full items-center justify-between text-sm">
+						{#if selectedRange.start && selectedRange.end}
+							{dateValueToString(selectedRange.start)} - {dateValueToString(selectedRange.end)}
+						{:else}
+							{$language === 'ru' ? 'Выберите период' : 'Select a range'}
+						{/if}
+						<Calendar class="size-4 opacity-50" />
+					</div>
+					<Separator class="mt-2 hidden group-hover:block"></Separator>
+					<RangeCalendar
+						bind:value={selectedRange}
+						weekStartsOn={1}
+						{numberOfMonths}
+						class="hidden capitalize group-hover:block"
+						onValueChange={handleRangeChange}
+						locale={$language === 'ru' ? 'ru' : 'en'}
+					/>
+				</div>
+			{/key}
+		{/if}
+	</div>
 
 	{#if scheduleStatus === 'error'}
 		<Alert.Root class="my-1 w-full min-w-[256px] md:w-2/3 lg:w-1/2 2xl:w-1/3">
@@ -399,73 +381,86 @@
 	{:else if scheduleStatus === 'loading'}
 		<Skeleton class="my-1 h-8 w-full min-w-[256px] md:w-2/3 lg:w-1/2 2xl:w-1/3" />
 	{:else if scheduleStatus === 'visible' && buildedSchedule}
-		{#each Object.entries(buildedSchedule) as [key, day]}
-			<div
-				class="mb-2 mt-1 w-full min-w-[256px] rounded-md border bg-background p-1 shadow-md md:w-2/3 lg:w-1/2 2xl:w-1/3"
-			>
-				<Table.Root>
-					<Table.Header
-						class="text-base {key === dateValueToString(today(getLocalTimeZone()))
-							? 'bg-green-100 dark:bg-green-800/50'
-							: studyDates[key]
-								? 'bg-blue-100 dark:bg-blue-800/50'
-								: 'mt-4 bg-red-100 p-4 dark:bg-red-800/50'}"
-					>
-						<Table.Row>
-							<Table.Head class="w-1/3 text-right font-semibold text-foreground">{key}</Table.Head>
-							<Table.Head class="w-2/3 text-left font-semibold text-foreground"
-								>{day.dayOfWeek}</Table.Head
-							>
-						</Table.Row>
-					</Table.Header>
-					<Table.Body class="bg-background">
-						{#if studyDates[key]}
-							{#if day.lessons[0].time !== Object.keys(timeIntervals)[0]}
-								<Table.Row>
-									<Table.Cell class="text-center font-semibold italic" colspan={2}>
-										{$language === 'ru' ? 'Пары начинаются с' : 'Lessons start at'}
-										{@const lessonTime = day.lessons[0]?.time}
+		<div
+			class="mx-auto my-1.5 grid grid-cols-1 justify-items-center gap-x-2 gap-y-3 md:p-2{Object.keys(
+				buildedSchedule
+			).length > 1
+				? ' lg:grid-cols-2 '
+				: ''}{Object.keys(buildedSchedule).length > 2 ? ' xl:grid-cols-3 ' : ''}{Object.keys(
+				buildedSchedule
+			).length > 3
+				? '2xl:grid-cols-4'
+				: ''}"
+		>
+			{#each Object.entries(buildedSchedule) as [key, day]}
+				<div
+					class="w-full min-w-[256px] max-w-[640px] rounded-md border bg-background p-1 shadow-md"
+				>
+					<Table.Root>
+						<Table.Header
+							class="text-base {key === dateValueToString(today(getLocalTimeZone()))
+								? 'bg-green-100 dark:bg-green-800/50'
+								: studyDates[key]
+									? 'bg-blue-100 dark:bg-blue-800/50'
+									: 'mt-4 bg-red-100 p-4 dark:bg-red-800/50'}"
+						>
+							<Table.Row>
+								<Table.Head class="w-1/3 text-right font-semibold text-foreground">{key}</Table.Head
+								>
+								<Table.Head class="w-2/3 text-left font-semibold text-foreground"
+									>{getDayOfWeek(key, $language)}</Table.Head
+								>
+							</Table.Row>
+						</Table.Header>
+						<Table.Body class="bg-background">
+							{#if studyDates[key]}
+								{#if day[0].time !== Object.keys(timeIntervals)[0]}
+									<Table.Row>
+										<Table.Cell class="text-center font-semibold italic" colspan={2}>
+											{$language === 'ru' ? 'Пары начинаются с' : 'Lessons start at'}
+											{@const lessonTime = day[0]?.time}
 
-										{#if lessonTime}
-											{@const time = lessonTime.includes('-')
-												? lessonTime.split('-')[0]
-												: lessonTime}
-											{time.includes('.') ? time.replace(/\./g, ':') : time}
-										{/if}
-									</Table.Cell>
-								</Table.Row>
+											{#if lessonTime}
+												{@const time = lessonTime.includes('-')
+													? lessonTime.split('-')[0]
+													: lessonTime}
+												{time.includes('.') ? time.replace(/\./g, ':') : time}
+											{/if}
+										</Table.Cell>
+									</Table.Row>
+								{/if}
+								{#each day as lesson}
+									<Table.Row>
+										<Table.Cell class="text-right"
+											>{lesson.time.includes('.')
+												? lesson.time.replace(/\./g, ':')
+												: lesson.time}</Table.Cell
+										>
+										<Table.Cell class="text-left">{lesson.discipline}</Table.Cell>
+									</Table.Row>
+									<Table.Row>
+										<Table.Cell class="text-right">
+											{#if lesson.auditorium.startsWith('http')}
+												<a href={lesson.auditorium} target="_blank" class="underline">
+													{$language === 'ru' ? 'Онлайн' : 'Online'}
+												</a>
+											{:else}
+												{lesson.auditorium}
+											{/if}
+										</Table.Cell>
+										<Table.Cell class="text-left">{lesson.teacher}</Table.Cell>
+									</Table.Row>
+								{/each}
+							{:else}
+								<Table.Cell class="text-center font-semibold italic" colspan={2}>
+									{$language === 'ru' ? 'Нет пар' : 'No lessons'}
+								</Table.Cell>
 							{/if}
-							{#each day.lessons as lesson}
-								<Table.Row>
-									<Table.Cell class="text-right"
-										>{lesson.time.includes('.')
-											? lesson.time.replace(/\./g, ':')
-											: lesson.time}</Table.Cell
-									>
-									<Table.Cell class="text-left">{lesson.discipline}</Table.Cell>
-								</Table.Row>
-								<Table.Row>
-									<Table.Cell class="text-right">
-										{#if lesson.auditorium.startsWith('http')}
-											<a href={lesson.auditorium} target="_blank" class="underline">
-												{$language === 'ru' ? 'Онлайн' : 'Online'}
-											</a>
-										{:else}
-											{lesson.auditorium}
-										{/if}
-									</Table.Cell>
-									<Table.Cell class="text-left">{lesson.teacher}</Table.Cell>
-								</Table.Row>
-							{/each}
-						{:else}
-							<Table.Cell class="text-center font-semibold italic" colspan={2}>
-								{$language === 'ru' ? 'Нет пар' : 'No lessons'}
-							</Table.Cell>
-						{/if}
-					</Table.Body>
-				</Table.Root>
-			</div>
-		{/each}
+						</Table.Body>
+					</Table.Root>
+				</div>
+			{/each}
+		</div>
 	{:else if scheduleStatus !== 'hidden'}
 		{#if lastQuery.year && lastQuery.group}
 			<Button

26e471fe2e814ca55bb75b2fc81a4cfb476a079e
Mapagmataas <98333916+Mapagmataas1331@users.noreply.github.com>
2024-11-21 01:24:12 +0700

    Minor improvements

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,15 @@
 <script lang="ts">
 	import Header from '$shared/components/Header.svelte';
 	import { Toaster } from '$shared/components/ui/sonner/index.js';
-	import { Separator } from '$shared/components/ui/separator/index.js';
+	import Footer from '$shared/components/Footer.svelte';
 	import Logo from '$lib/components/Logo.svelte';
 	import '@/app.scss';
-	import { language } from '$shared/stores/language';
 
 	let { children } = $props();
 </script>
 
 <Header
-	navLinks={{
+	links={{
 		// '/home/': { name: 'Home', nameRu: 'Главная' },
 		'/': { name: 'Student', nameRu: 'Студенту' }
 		// '/teacher/': { name: 'Teacher', nameRu: 'Преподавателю' }
@@ -25,21 +24,17 @@
 
 {@render children()}
 
-<footer class="flex flex-wrap justify-center gap-2 px-2 py-4 text-xs text-muted-foreground">
-	<a href="https://ma.cyou/" class="hover:text-foreground"
-		>{$language === 'ru' ? 'Сайт Автора' : "Author's site"}</a
-	>
-	<Separator orientation="vertical" class="bg-muted-foreground bg-opacity-50" />
-	<a href="https://opencollege-nsk.ru/" class="hover:text-foreground"
-		>{$language === 'ru' ? "Сайт НГОК'а" : "NGOK's site"}</a
-	>
-	<Separator orientation="vertical" class="bg-muted-foreground bg-opacity-50" />
-	<a
-		href="https://docs.google.com/spreadsheets/d/1FiMov0r4UUDKT6A56NWMImpoUakDC2YDevgaOpJQ7Qc/"
-		class="hover:text-foreground">{$language === 'ru' ? 'Таблица расписания' : 'Schedule table'}</a
-	>
-	<Separator orientation="vertical" class="bg-muted-foreground bg-opacity-50" />
-	<a href="https://github.com/Mapagmataas1331/NGOK-Better-schedule" class="hover:text-foreground"
-		>{$language === 'ru' ? 'Исходный код' : 'Source code'}</a
-	>
-</footer>
+<Footer
+	links={{
+		'https://ma.cyou/': { name: "Author's site", nameRu: 'Сайт Автора' },
+		'https://opencollege-nsk.ru/': { name: "NGOK's site", nameRu: "Сайт НГОК'а" },
+		'https://docs.google.com/spreadsheets/d/1FiMov0r4UUDKT6A56NWMImpoUakDC2YDevgaOpJQ7Qc/': {
+			name: 'Schedule table',
+			nameRu: 'Таблица расписания'
+		},
+		'https://github.com/Mapagmataas1331/NGOK-Better-schedule': {
+			name: 'Source code',
+			nameRu: 'Исходный код'
+		}
+	}}
+/>
